108e6b9c50d0e25b13aba1ddafd77184fb64eeca
David La Chasse <david.lachasse@gmail.com>
2016-09-23 10:15:04 -0700

    Use Net::HTTP for timeout handling

--- lib/owd/error.rb
+++ lib/owd/error.rb
@@ -2,8 +2,8 @@ module OWD
   class APIError < StandardError
     attr_accessor :owd_error_type, :owd_error_message
     def initialize details = {}
-      self.owd_error_type    = details['type']
-      self.owd_error_message = details['message']
+      self.owd_error_type    = details.fetch('type', nil)
+      self.owd_error_message = details.fetch('message', nil)
       super("#{owd_error_type}: #{owd_error_message}")
     end
   end
--- lib/owd/request.rb
+++ lib/owd/request.rb
@@ -2,30 +2,44 @@ module OWD
   class Request
     ENDPOINT = 'https://secure.owd.com/webapps/api/api.jsp'
 
-    attr_reader :xml, :timeout_seconds
+    attr_reader :xml, :timeout_seconds, :uri
 
     def initialize(xml, timeout_seconds)
       @xml = xml
       @timeout_seconds = timeout_seconds.to_i
+      @uri = URI.parse(ENDPOINT)
     end
 
     def perform
-      uri = URI.parse(ENDPOINT)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = (uri.scheme == 'https')
-
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.body = xml
-      request["Content-Type"] = "text/xml"
-
-      Timeout::timeout(timeout_seconds) {
-        parse_response(http.request(request))
-      }
+      parse_response
+    rescue Net::OpenTimeout
+      raise APIError.new({
+        'type' => 'Timeout',
+        'message' => 'Request timed out'})
     end
 
     private
 
-    def parse_response(response)
+    def client
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == 'https')
+      http.open_timeout = timeout_seconds
+      http.read_timeout = timeout_seconds
+      http
+    end
+
+    def request_parameters
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.body = xml
+      request["Content-Type"] = "text/xml"
+      request
+    end
+
+    def response
+      @response ||= client.request(request_parameters)
+    end
+
+    def parse_response
       Crack::XML.parse(response.body)['OWD_API_RESPONSE'].tap do |results|
         if results['results'] == 'ERROR'
           raise APIError.new 'type'    => results['error_type'],
