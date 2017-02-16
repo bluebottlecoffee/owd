324a32267beaca5b3a57de55a7e738b4ded8d555
Keith Lawler <keithlawler@me.com>
2017-02-15 17:57:02 -0800

    Order Update: add optional line_items

--- test/documents/test_order_update.rb
+++ test/documents/test_order_update.rb
@@ -38,5 +38,46 @@ describe OWD::OrderUpdate do
         </OWD_API_REQUEST>
       XML
     end
+
+    it 'builds an XML body with line_items if present' do
+      assert_equal_xml @doc.build(
+        order_reference: 123,
+        first_name: "Joe",
+        address_one: "300 Webster Street",
+        city: "Oakland",
+        state: "CA",
+        email: "email@gmail.com",
+        zip: "94607",
+        country: "US",
+        phone: "555-5555",
+        line_items: [
+          part_reference: 'SKU',
+          description: 'Material Name',
+          requested: 11,
+          cost: '1.5',
+          declared_value: '1.0',
+          customs_desc: 'Customs description',
+          line_number: '123-line-item',
+          is_insert_item: 'NO'
+        ]
+      ), <<-XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <OWD_API_REQUEST>
+          <OWD_ORDER_UPDATE_REQUEST clientOrderId="123">
+            <SHIP_NAME>Joe</SHIP_NAME>
+            <SHIP_ADDRESS_ONE>300 Webster Street</SHIP_ADDRESS_ONE>
+            <SHIP_CITY>Oakland</SHIP_CITY>
+            <SHIP_STATE>CA</SHIP_STATE>
+            <SHIP_POSTCODE>94607</SHIP_POSTCODE>
+            <SHIP_COUNTRY>US</SHIP_COUNTRY>
+            <SHIP_PHONE>555-5555</SHIP_PHONE>
+            <SHIP_EMAIL>email@gmail.com</SHIP_EMAIL>
+            <LINE_ITEMS>
+              <LINE_ITEM part_reference="SKU" description="Material Name" requested="11" cost="1.5" declared_value="1.0" customs_desc="Customs description" line_number="123-line-item" is_insert_item="NO" />
+            </LINE_ITEMS>
+          </OWD_ORDER_UPDATE_REQUEST>
+        </OWD_API_REQUEST>
+      XML
+    end
   end
 end
